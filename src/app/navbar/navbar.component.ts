fa9a86d6c87060a77d40dd3ed4b79dc6e2979dce
Mohammadalawia <109400462+Mohammadalawia@users.noreply.github.com>
2024-08-31 22:16:32 +0300

    navbar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+    isHr(): boolean{
+      return true;
+    }
+}
